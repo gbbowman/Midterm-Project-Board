49ddf27410263de4c28a1be9e26eb2c1e6780942
Kai Blumberg <kblumberg@email.arizona.edu>
2019-10-08 19:51:52 -0700

    Modify script for tests to run

--- midterm_code/protein_seq/run.sh
+++ midterm_code/protein_seq/run.sh
@@ -7,7 +7,5 @@
 
 ./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 1 --outdir protein_Test1
 ./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 5 --outdir protein_Test2
-./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 10 --outdir protein_Test3
 ./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 50 --outdir protein_Test4
 ./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 100 --outdir protein_Test5
-./seq_count.py uniprot-proteome%3AUP000005640.fasta --int 500 --outdir protein_Test6
