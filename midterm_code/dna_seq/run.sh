de05ab582dec852d8d59378eac8cb652208763fd
Kai Blumberg <kblumberg@email.arizona.edu>
2019-10-31 15:08:06 -0700

    Fix run.sh

--- midterm_code/dna_seq/run.sh
+++ midterm_code/dna_seq/run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-wget https://sra-pub-src-1.s3.amazonaws.com/SRR9586219/Et1.fastq.1
-mv Et1.fastq.1 dna.fasta
-
-./seq_count.py dna.fasta --int 3 --outdir DNA_Test1
-./seq_count.py dna.fasta --int 5 --outdir DNA_Test2
-./seq_count.py dna.fasta --int 25 --outdir DNA_Test3
-./seq_count.py dna.fasta --int 247 --outdir DNA_Test4
-./seq_count.py dna.fasta --int 1235 --outdir DNA_Test5
-
-rm dna.fasta
+./seq_count.py dna.fasta --int 3 --outdir dna_1000
+./seq_count.py dna.fasta --int 5 --outdir dna_2000
+./seq_count.py dna.fasta --int 12 --outdir dna_5000
+./seq_count.py dna.fasta --int 25 --outdir dna_10000
+./seq_count.py dna.fasta --int 124 --outdir dna_50000
+./seq_count.py dna.fasta --int 247 --outdir dna_100000
+./seq_count.py dna.fasta --int 1235 --outdir dna_500000
