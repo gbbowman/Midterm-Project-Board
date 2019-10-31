976c2f9afe84155ad4f58b50e5ec5dae687a7654
Kai Blumberg <kblumberg@email.arizona.edu>
2019-10-31 15:09:07 -0700

    Fix run.sh

--- midterm_code/protein_seq/run.sh
+++ midterm_code/protein_seq/run.sh
@@ -5,7 +5,8 @@
 # available from https://www.uniprot.org/uniprot/?query=proteome:UP000005640
 # which was found from https://www.uniprot.org/help/reference_proteome
 
-./seq_count.py protein.fasta --int 1 --outdir protein_Test1
-./seq_count.py protein.fasta --int 5 --outdir protein_Test2
-./seq_count.py protein.fasta --int 50 --outdir protein_Test4
-./seq_count.py protein.fasta --int 100 --outdir protein_Test5
+./seq_count.py protein.fasta --int 1 --outdir protein_1
+./seq_count.py protein.fasta --int 5 --outdir protein_5
+./seq_count.py protein.fasta --int 50 --outdir protein_50
+./seq_count.py protein.fasta --int 100 --outdir protein_100
+./seq_count.py protein.fasta --int 500 --outdir protein_500
